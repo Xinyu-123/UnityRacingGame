9c051756b252991732c89ffc3a3ca90503539f08
Xinyu <xinyudong1@gmail.com>
2019-09-06 18:28:43 -0400

    Fixed Camera stutter, Created Track, and added Trees

---
+++ Assets/Scripts/CameraStable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour
+{
+
+    public GameObject TheCar;
+    public float CarX, CarY, CarZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CarX = TheCar.transform.eulerAngles.x;
+        CarY = TheCar.transform.eulerAngles.y;
+        CarZ = TheCar.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(0, CarY,0);
+
+    }
+}
