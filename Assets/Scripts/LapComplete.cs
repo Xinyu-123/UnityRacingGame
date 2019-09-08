041b6ca5e947858b48bbaee03fa84ec875cd003e
Xinyu <xinyudong1@gmail.com>
2019-09-08 12:49:31 -0400

    Completed Lap Times

---
+++ Assets/Scripts/LapComplete.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LapComplete : MonoBehaviour
+{
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+
+    public GameObject MinuteDisplay, SecondDisplay, MilliDisplay, LapTimeBox;
+
+    private void OnTriggerEnter()
+    {
+
+        if (LapTimeManager.MinuteCount <= 9)
+        {
+            MinuteDisplay.GetComponent<TextMeshProUGUI>().text = "0" + LapTimeManager.MinuteCount + ":";
+        }
+        else
+        {
+            MinuteDisplay.GetComponent<TextMeshProUGUI>().text = "" + LapTimeManager.MinuteCount + ":";
+        }
+
+        if(LapTimeManager.SecondCount <= 9)
+        {
+            SecondDisplay.GetComponent<TextMeshProUGUI>().text = "0" + LapTimeManager.SecondCount + ".";
+        }
+        else
+        {
+            SecondDisplay.GetComponent<TextMeshProUGUI>().text = "" + LapTimeManager.SecondCount + ".";
+        }
+
+        MilliDisplay.GetComponent<TextMeshProUGUI>().text = "" + LapTimeManager.MilliDisplay;
+
+        LapTimeManager.MinuteCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MilliCount = 0;
+
+        HalfLapTrig.SetActive(true);
+        LapCompleteTrig.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/LapTimeManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LapTimeManager : MonoBehaviour
+{
+    public static int MinuteCount, SecondCount;
+    public static float MilliCount;
+    public static string MilliDisplay;
+
+    public GameObject MinuteBox, SecondBox, MilliBox;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MilliCount += Time.deltaTime * 10;
+        MilliDisplay = MilliCount.ToString("F0");
+        MilliBox.GetComponent<TextMeshProUGUI>().text = "" + MilliDisplay;
+
+        if(MilliCount >= 9)
+        {
+            MilliCount = 0;
+            SecondCount++;
+        }
+
+        if(SecondCount <= 9)
+        {
+            SecondBox.GetComponent<TextMeshProUGUI>().text = "0" + SecondCount + ".";
+        }
+        else
+        {
+            SecondBox.GetComponent<TextMeshProUGUI>().text = "" + SecondCount + ".";
+        }
+
+        if(SecondCount >= 60)
+        {
+            MinuteCount++;
+            SecondCount = 0;
+        }
+
+        if(MinuteCount <= 9)
+        {
+            MinuteBox.GetComponent<TextMeshProUGUI>().text = "0" + MinuteCount + ":";
+        }
+        else
+        {
+            MinuteBox.GetComponent<TextMeshProUGUI>().text = "" + MinuteCount + ":";
+        }
+    }
+}
