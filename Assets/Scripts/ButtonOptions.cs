aeff090cb60a8fdc1958b95857f8a66d5b7e307d
Xinyu <xinyudong1@gmail.com>
2019-09-14 15:47:42 -0400

    Created Main Menu and Bug fixes

---
+++ Assets/Scripts/ButtonOptions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOptions : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void TrackSelect()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void CharacterSelect()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //Below are Track Select Buttons
+    public void Track01()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
