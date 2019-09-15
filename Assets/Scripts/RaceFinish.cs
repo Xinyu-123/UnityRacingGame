b2fd778c500e43794379375109160075c2e8c933
Xinyu <xinyudong1@gmail.com>
2019-09-15 13:56:57 -0400

    Manual Reset

--- Assets/Scripts/RaceFinish.cs
+++ Assets/Scripts/RaceFinish.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityStandardAssets.Vehicles.Car;
 
@@ -7,8 +8,45 @@ public class RaceFinish : MonoBehaviour
 {
     public GameObject MyCar, FinishCam, ViewModes, CompleteTrig;
     public AudioSource LevelMusic, FinishMusic;
+
+    public GameObject MinuteDisplay, SecondDisplay, MilliDisplay, LapTimes;
+
+    public int Laps;
+
+    public float RawTime;
+
     private void OnTriggerEnter(Collider other)
     {
+        RawTime = PlayerPrefs.GetFloat("RawTime");
+        if (LapTimeManager.RawTime < RawTime || RawTime == 0)
+        {
+            if (LapTimeManager.MinuteCount <= 9)
+            {
+                MinuteDisplay.GetComponent<TextMeshProUGUI>().text = "0" + LapTimeManager.MinuteCount + ":";
+            }
+            else
+            {
+                MinuteDisplay.GetComponent<TextMeshProUGUI>().text = "" + LapTimeManager.MinuteCount + ":";
+            }
+
+            if (LapTimeManager.SecondCount <= 9)
+            {
+                SecondDisplay.GetComponent<TextMeshProUGUI>().text = "0" + LapTimeManager.SecondCount + ".";
+            }
+            else
+            {
+                SecondDisplay.GetComponent<TextMeshProUGUI>().text = "" + LapTimeManager.SecondCount + ".";
+            }
+
+            MilliDisplay.GetComponent<TextMeshProUGUI>().text = "" + LapTimeManager.MilliDisplay;
+
+            PlayerPrefs.SetInt("MinSave", LapTimeManager.MinuteCount);
+            PlayerPrefs.SetInt("SecSave", LapTimeManager.SecondCount);
+            PlayerPrefs.SetFloat("MilliSave", LapTimeManager.MilliCount);
+            PlayerPrefs.SetFloat("RawTime", LapTimeManager.RawTime);
+        }
+
+
         this.GetComponent<BoxCollider>().enabled = false;
         MyCar.SetActive(false);
         CompleteTrig.SetActive(false);
@@ -20,5 +58,6 @@ public class RaceFinish : MonoBehaviour
         ViewModes.SetActive(false);
         LevelMusic.Stop();
         FinishMusic.Play();
+        LapTimes.SetActive(false);
     }
 }
